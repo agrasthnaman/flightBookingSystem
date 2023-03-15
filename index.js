a248b7f74d33cdfc27a57fa2705d07fb90397ad3
Agrasth <agrasthnaman.an@gmail.com>
2023-03-15 22:12:51 +0530

    swagger snippet added

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
+const express = require('express');
+const swaggerUi = require('swagger-ui-dist');
+const app = express();
+
+app.use('/docs', express.static(swaggerUi.getAbsoluteFSPath()));
+app.get('/docs', (req, res) => {
+    res.sendFile(swaggerUi.getAbsoluteFSPath('index.html'));
+});
 const { dbConnectAvailableFlights, dbConnectBookings } = require('./mongodb');
 
 const main  = async () =>{
